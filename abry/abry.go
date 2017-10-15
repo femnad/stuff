da852156208085a5ba90b986244e9d96430aee88
Fahri Cihan Demirci <femnad@users.noreply.github.com>
2017-10-15 13:45:40 +0300

    Apply gofmt

--- abry/abry.go
+++ abry/abry.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
-    "strings"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
 )
 
 const abbreviationsFile = "~/.config/fish/functions/__fish_abbreviations.fish"
@@ -12,47 +12,47 @@ const abbreviationCommand = "abbr"
 const tempFile = "/tmp/fish_abbrevs.fish"
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
-func expandHome(path string) (string) {
-    home := os.Getenv("HOME")
-    return strings.Replace(path, "~", home, 1)
+func expandHome(path string) string {
+	home := os.Getenv("HOME")
+	return strings.Replace(path, "~", home, 1)
 }
 
 func printUsage() {
-    fmt.Printf("usage: abry <abbreviation> <abbreviation-definition>\n")
+	fmt.Printf("usage: abry <abbreviation> <abbreviation-definition>\n")
 }
 
-func getTokenWithIndex(line string, tokenIndex int) (string) {
-    trimmedLine := strings.TrimSpace(line)
-    tokens := strings.Split(trimmedLine, " ")
-    return tokens[tokenIndex]
+func getTokenWithIndex(line string, tokenIndex int) string {
+	trimmedLine := strings.TrimSpace(line)
+	tokens := strings.Split(trimmedLine, " ")
+	return tokens[tokenIndex]
 }
 
-func getFirstToken(line string) (string) {
-    return getTokenWithIndex(line, 0)
+func getFirstToken(line string) string {
+	return getTokenWithIndex(line, 0)
 }
 
-func getAbbrevName(line string) (string) {
-    return getTokenWithIndex(line, 2)
+func getAbbrevName(line string) string {
+	return getTokenWithIndex(line, 2)
 }
 
-func isAbbreviationLine(line string) (bool) {
-    firstToken := getFirstToken(line)
-    if firstToken == abbreviationCommand {
-        return true
-    }
-    return false
+func isAbbreviationLine(line string) bool {
+	firstToken := getFirstToken(line)
+	if firstToken == abbreviationCommand {
+		return true
+	}
+	return false
 }
 
-func getAbbrevCommand(abbrName string, abbrPhrase string) (string) {
-    return fmt.Sprintf("    abbr --add %s %s\n", abbrName, abbrPhrase)
+func getAbbrevCommand(abbrName string, abbrPhrase string) string {
+	return fmt.Sprintf("    abbr --add %s %s\n", abbrName, abbrPhrase)
 }
 
-func maybeWriteNewAbbreviation(line string, abbrName string, abbrPhrase string, writer *bufio.Writer) (bool) {
+func maybeWriteNewAbbreviation(line string, abbrName string, abbrPhrase string, writer *bufio.Writer) bool {
 	existingAbbrev := getAbbrevName(line)
 	if existingAbbrev > abbrName {
 		abbrevCommand := getAbbrevCommand(abbrName, abbrPhrase)
@@ -63,46 +63,46 @@ func maybeWriteNewAbbreviation(line string, abbrName string, abbrPhrase string, 
 }
 
 func addAbreviation(abbrName string, abbrPhrase string) {
-    expanded := expandHome(abbreviationsFile)
-    abbrevsFile, err := os.Open(expanded)
-    check(err)
-    defer abbrevsFile.Close()
+	expanded := expandHome(abbreviationsFile)
+	abbrevsFile, err := os.Open(expanded)
+	check(err)
+	defer abbrevsFile.Close()
 
-    reader := bufio.NewReader(abbrevsFile)
-    check(err)
+	reader := bufio.NewReader(abbrevsFile)
+	check(err)
 
-    tempFile, err := os.Create(tempFile)
-    check(err)
-    defer tempFile.Close()
+	tempFile, err := os.Create(tempFile)
+	check(err)
+	defer tempFile.Close()
 
-    writer := bufio.NewWriter(tempFile)
+	writer := bufio.NewWriter(tempFile)
 
-    found := false
-    for {
-        line, err := reader.ReadString('\n')
-        if isAbbreviationLine(line) && !found {
+	found := false
+	for {
+		line, err := reader.ReadString('\n')
+		if isAbbreviationLine(line) && !found {
 			found = maybeWriteNewAbbreviation(line, abbrName, abbrPhrase, writer)
-        }
+		}
 		writer.WriteString(line)
-        if err != nil {
-            break
-        }
-    }
+		if err != nil {
+			break
+		}
+	}
 
-    writer.Flush()
+	writer.Flush()
 }
 
 func main() {
-    args := os.Args
-    numberOfArgs := len(args)
+	args := os.Args
+	numberOfArgs := len(args)
 
-    if numberOfArgs < 3 {
-        printUsage()
-        os.Exit(1)
-    }
+	if numberOfArgs < 3 {
+		printUsage()
+		os.Exit(1)
+	}
 
-    abbrName := args[1]
-    abbrPhrase := strings.Join(args[2:], " ")
+	abbrName := args[1]
+	abbrPhrase := strings.Join(args[2:], " ")
 
 	addAbreviation(abbrName, abbrPhrase)
 }
